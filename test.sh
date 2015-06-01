8e20b2a03fcdac42e4b8825a8ac5589830ddcf13
thrashr888 <thrashr888@gmail.com>
2015-06-01 11:38:45 -0700

    update tests

--- test.sh
+++ test.sh
@@ -1,11 +1,44 @@
+#!/bin/bash
+
+
+# Cleanup
+rm -f crash.log
+rm -f p2t
+
 
 # Testing
 go test ./...
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
 
 # Linting
-go vet */*.go
-go vet *.go
-golint */*.go
-golint *.go
-gocyclo -over 10 */*.go
-gocyclo -over 10 *.go
+go vet ./...
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+golint ./...
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+gocyclo -over 15 .
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+
+# Formatting
+gofmt -s -d -l */*.go
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+
+# Build
+go build -o p2t
+if [[ $? != 0 ]]; then
+    exit 1
+fi
